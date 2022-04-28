e70e810680a4c8d9f5731550360dccad915b2225
Jonno Clifford <jma.clifford@gmail.com>
2022-04-28 20:46:41 +0100

    All rakes passed

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,52 @@
+class ReviewsController < ApplicationController
+  # all reviews
+  def index
+    @reviews = Review.all
+  end
+
+  # one review
+  def show
+    # @review = review.find(review_params)
+    @review = Review.find(params[:id])
+  end
+
+  # new review
+  def new
+    # we need @restaurant in our `simple_form_for`
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    @review.update(review_params)
+    redirect_to review_path(@review)
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to reviews_path
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants
+  resources :restaurants do
+  resources :reviews, only: [:new, :create]
+  end
 end
+
+
+  # # Read all restaurants
+  # get '/restaurants', to: 'restaurants#index'
+  # # Add new restaurant
+  # get '/restaurants/new', to: 'restaurants#new' # displays the form
+  # post '/restaurants', to: 'restaurants#create'
+  # # update a restaurant
+  # get '/restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  # patch '/restaurants/:id', to: 'restaurants#update'
+  # # delete a restaurant
+  # delete '/restaurants/:id', to: 'restaurants#destroy'
